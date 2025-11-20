af453957f470787c8ac584c5d5ef423f9967259c
AZIZ-Soufiane <AZIZ.Soufiane.Solicode@gmail.com>
2025-11-20 09:51:36 +0000

    LiveCoding Done

---
+++ LiveCodingPrototype/Prototype/resources/views/layouts/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Articles</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="bg-gray-100 text-gray-900 min-h-screen">
+    <div class="max-w-6xl mx-auto py-10 px-4">
+        @yield('content')
+    </div>
+</body>
+</html>
